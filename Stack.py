5d6ffed62f9f07c24e3cbde874683683fb2c28f1
Omar Sarabia <elomra@gmail.com>
2014-06-11 19:23:55 -0700

    Implementing Stack ADT

--- Stack.py
+++ Stack.py
@@ -4,3 +4,33 @@ class Stack:
 
   def __init__(self):
     self.items = []
+
+  def push(self,item):
+    self.items.append(item)
+
+  def pop(self):
+    return self.items.pop()
+
+  def peek(self):
+    return self.items[-1]
+
+  def isEmpty(self):
+    return self.items == []
+
+  def size(self):
+    return len(self.items)
+
+##
+
+stack = Stack()
+
+stack.push(5)
+stack.push(6)
+
+print stack.peek()
+
+print stack.pop()
+print stack.pop()
+
+print "isEmpty:"
+print stack.isEmpty()
